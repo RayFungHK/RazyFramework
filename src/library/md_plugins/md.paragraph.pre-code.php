82f278ca4ee9c3d2b02f6d98d5f07f253e8c055a
Ray Fung <hello@rayfung.hk>
2018-08-04 01:48:45 +0800

    v0.13 build 41 Markdown plugin md.paragraph.pre-spaces has merged into md.paragraph.pre-code because pre-spaces may execute before pre-code and convert the content start with four spaces in the pre-code incorrectly.

--- src/library/md_plugins/md.paragraph.pre-code.php
+++ src/library/md_plugins/md.paragraph.pre-code.php
@@ -1,10 +1,14 @@
 <?php
 return [
-  'pattern' => '\h{0,3}```(\w*\h*)\n(.+?)(?>\n```(?!\h{4})|\Z)',
+  'pattern' => '(?>\h{0,3}```(\w*\h*)\n(.+?)(?>\n```(?!\h{4})|\Z)|((?>    |\t)[^\n]+\n?)+)',
   'callback' => function($matches) {
-    $content = $matches[2];
-    $content = preg_replace('/^\R+|\R+$/', '', $content);
-    return '<pre' . (($matches[1]) ? ' language="' . $matches[1] . '"' : '') . '><code>' . htmlspecialchars($content) . '</code></pre>';
+    if (isset($matches[3]))  {
+      $content = preg_replace('/(?<=\n|\A)(?:    |\t)/', '',  $matches[0]);
+      return '<pre>' . htmlspecialchars(trim($content)) . '</pre>';
+    } else {
+      $content = preg_replace('/^\n+|\n+$/', '', $matches[2]);
+      return '<pre' . (($matches[1]) ? ' language="' . $matches[1] . '"' : '') . '><code>' . htmlspecialchars($content) . '</code></pre>';
+    }
   }
 ];
 ?>
